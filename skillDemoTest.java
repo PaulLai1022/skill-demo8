2eb2d62303c40ea448684f47ac79fc73368435e7
PaulLai1022 <71100835+PaulLai1022@users.noreply.github.com>
2022-05-11 12:10:56 -0700

    fail test input + lib

---
+++ skillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillDemoTest {
+    @Test
+    public void subtraction() {
+        assertEquals(1, skillDemo.subtraction(4,2));
+    }
+}
